551adacc41253a9eb9af966b5f07c69d65e849ce
Rohit Panjaitan <rohitpanjaitan7@gmail.com>
2025-10-07 10:51:09 +0700

    fix : adjust role access

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,21 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userController = require('../controllers/userController');
-const auth = require('../middleware/auth');
+const userController = require("../controllers/userController");
+const auth = require("../middleware/auth");
 
 // Get all users
-router.get('/', auth.isAuthenticated, userController.getAllUsers);
+router.get("/", auth.isAuthenticated, userController.getAllUsers);
 
 // Get all user roles
-router.get('/roles', auth.isAuthenticated, userController.getAllRoles);
+router.get("/roles", auth.isAuthenticated, userController.getAllRoles);
 
 // Create new user
-router.post('/', auth.isAuthenticated, userController.createUser);
+router.post("/", auth.isAuthenticated, userController.createUser);
 
 // Update user
-router.put('/:id', auth.isAuthenticated, userController.updateUser);
+router.put("/:id", auth.isAuthenticated, userController.updateUser);
 
 // Delete user
-router.delete('/:id', auth.isAuthenticated, userController.deleteUser);
+router.delete("/:id", auth.isAuthenticated, userController.deleteUser);
 
 module.exports = router;
